885850854fd8225cd98e11956eaf8258dec6829e
albertegi <albertegi90@gmail.com>
2026-01-20 23:59:00 +0000

    update PaymentController ** Injected PaymentService ** created a PostMapping method to receive the PaymentRequest

--- services/payment/src/main/java/com/alvirg/ecommerce/payment/PaymentController.java
+++ services/payment/src/main/java/com/alvirg/ecommerce/payment/PaymentController.java
@@ -1,6 +1,10 @@
 package com.alvirg.ecommerce.payment;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class PaymentController {
 
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Integer> createPayment(
+            @RequestBody @Valid
+            PaymentRequest request
+    ){
+
+    }
 }
