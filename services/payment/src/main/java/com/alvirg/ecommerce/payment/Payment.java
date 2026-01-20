25b2321811fec100201315c947b2a9d2568b0cce
albertegi <albertegi90@gmail.com>
2026-01-20 23:25:15 +0000

    add required annotation on the Payment entity

--- services/payment/src/main/java/com/alvirg/ecommerce/payment/Payment.java
+++ services/payment/src/main/java/com/alvirg/ecommerce/payment/Payment.java
@@ -1,4 +1,18 @@
 package com.alvirg.ecommerce.payment;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "table")
 public class Payment {
 }
