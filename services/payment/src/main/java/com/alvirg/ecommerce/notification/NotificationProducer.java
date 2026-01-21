4852e675546ae1b9270c4c14739f7b0ff2801a8f
albertegi <albertegi90@gmail.com>
2026-01-21 02:28:29 +0000

    implement sendNotification method ** received PaymentNotification as request ** parameterized logged info to display the incoming request ** create and object of Message from the spring framework of the PaymentNotificationRequest with a Builder ** Construct the body builder with the request and  topic ** then send the message

---
+++ services/payment/src/main/java/com/alvirg/ecommerce/notification/NotificationProducer.java
@@ -0,0 +1,29 @@
+package com.alvirg.ecommerce.notification;
+
+import com.alvirg.ecommerce.payment.Payment;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationProducer {
+
+    private final KafkaTemplate<String, PaymentNotificationRequest> kafkaTemplate;
+
+    public void sendNotification(PaymentNotificationRequest request){
+        log.info("Sending notification with body <{}>", request);
+        // create and object of type Message of the PaymentNotificationRequest from spring framework
+        Message<PaymentNotificationRequest> message = MessageBuilder
+                .withPayload(request)
+                .setHeader(KafkaHeaders.TOPIC, "payment-topic")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
