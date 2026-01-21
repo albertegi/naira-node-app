a6c73879e67d02929e23b1f864128bcdd463e4ee
albertegi <albertegi90@gmail.com>
2026-01-21 00:02:21 +0000

    Create PaymentService and add required annotations to the service

---
+++ services/payment/src/main/java/com/alvirg/ecommerce/payment/PaymentService.java
@@ -0,0 +1,9 @@
+package com.alvirg.ecommerce.payment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+}
